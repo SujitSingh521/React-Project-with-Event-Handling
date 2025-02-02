cf0b12a400ac02dc6888a3c88c0d6c355b2913dc
Sujit Kumar <147229100+SujitSingh521@users.noreply.github.com>
2025-02-02 08:06:24 +0000

    updated

--- react-event/src/components/LearnEvent.jsx
+++ react-event/src/components/LearnEvent.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 
 const LearnEvent = () => {
-    const handleClick1 =(e)=>{
+    const handleClick1 = (e) => {
         e.preventDefault()
         console.log("Button Clicked 1");
     }
-  return (
-    <div>
-      <button onClick={handleClick1}> Click1</button>
-    </div>
-  )
+    const handleClick2 = (myId) => {
+
+        console.log("Button Clicked 2", myId);
+    }
+    return (
+        <div>
+            {/* Without Argument */}
+            <button onClick={handleClick1}> Click1</button>
+            <br />
+            {/* With Argument */}
+            <button onClick={(e) => handleClick2("myId")}>Click2</button>
+        </div>
+    )
 }
 
 export default LearnEvent
